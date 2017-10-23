6181b6ca114b8e4b74e798f0aa685bd64f99987d
Peter <peter.muryshkih@iais.fraunhofer.de>
2017-10-23 08:59:27 +0200

    fixes to wait script

--- swarm/examples/scripts/wait-for-hadoop.sh
+++ swarm/examples/scripts/wait-for-hadoop.sh
@@ -3,14 +3,14 @@
 echo "Check HDFS connectivity with telnet"
 function wait_telnet()
 {
-   
-    
+
+
     local service=$1
     local port=$2
     local retry_seconds=5
     local max_try=100
     let i=1
-    result=$(echo exit | telnet namenode 50070 2>&1 | grep -i  "Connection refused"  | wc -l)
+    result=$(echo exit | telnet ${service} ${port} 2>&1 | grep -i  "Connection refused"  | wc -l)
     until [ $result -eq 0 ]; do
       echo "[$i/$max_try] check for ${service}..."
       echo "[$i/$max_try] ${service} is not available yet"
@@ -18,21 +18,17 @@ function wait_telnet()
         echo "[$i/$max_try] ${service} is still not available; giving up after ${max_try} tries. :/"
         exit 1
       fi
-      
+
       echo "[$i/$max_try] try in ${retry_seconds}s once again ..."
       let "i++"
       sleep $retry_seconds
 
-      result=$(echo exit | telnet namenode 50070 2>&1 | grep -i  "Connection refused"  | wc -l)
-    
+      result=$(echo exit | telnet ${service} ${port} 2>&1 | grep -i  "Connection refused"  | wc -l)
+
     done
-    echo "[$i/$max_try] $service is available by telnet."
+    echo "[$i/$max_try] ${service} is available by telnet. Check OK."
 
 }
 
-SERVICE_PRECONDITION=("namenode")
 
-for i in "${SERVICE_PRECONDITION[@]}"
-do
-    wait_telnet ${i}
-done
+wait_telnet "namenode"
